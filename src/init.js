395a7603055652c0f1c83c6584b9166e91de2354
SamroodAli <samroodali@live.com>
2021-02-05 19:01:50 +0530

    Init js to initialise game state and ticker every 3 seconds

---
+++ src/init.js
@@ -0,0 +1,28 @@
+const TICK_RATE = 3000;
+
+function tick() {
+  console.log("tick", Date.now());
+}
+
+function init() {
+  console.log("starting game");
+
+  let nextTimeToTick = Date.now();
+
+  // recursive function to che
+  function nextAnimationFrame() {
+    const now = Date.now();
+
+    if (nextTimeToTick <= now) {
+      tick();
+      // requestAnimationFrame is going to be called a lot, so we put to condition to tick only in TICK_RATE time. here 3 seconds
+      nextTimeToTick = now + TICK_RATE;
+    }
+    // using browser API requestAnimation frame to keep running nextAnimationFrame
+    requestAnimationFrame(nextAnimationFrame);
+  }
+  // calling the first tick
+  requestAnimationFrame(nextAnimationFrame);
+}
+
+init();
