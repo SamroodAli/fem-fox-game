d0d3d3bfb12aa89c0403a06b2ba46c77630f184d
SamroodAli <samroodali@live.com>
2021-02-07 20:44:07 +0530

    Fix:constant file tick rate  fixed to 3 seconds

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-export const TICK_RATE = 1000;
+export const TICK_RATE = 3000;
 export const ICONS = ["fish", "poop", "weather"];
 export const RAIN_CHANCE = 0.2;
 export const SCENES = ["day", "rain"];
